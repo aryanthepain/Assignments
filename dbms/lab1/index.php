a1987665577b933e850159ac6241372805be553f
aryanthepain <aryan.gupta@iitg.ac.in>
2025-01-21 04:00:13 +0530

    lab 1 basic student db

---
+++ dbms/lab1/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lab 1</title>
+    <link rel="stylesheet" href="css/form.css">
+</head>
+
+<body margin="0%">
+    <?php
+    // all the forms
+    include("forms/insertForm.php");
+    include("forms/selectForm.php");
+    include("forms/deleteForm.php");
+    include("forms/modifyForm.php");
+    ?>
+</body>
+
+</html>

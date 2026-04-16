96c0f2b12e72646ddec2cdf73d54613f21aa5185
Aryan Gupta <aryan.gupta@iitg.ac.in>
2026-04-16 10:59:16 +0530

    Stop tracking dbms and intro_to_ml directories

--- dbms/lab2/functions/display_alert.php
+++
@@ -1,8 +0,0 @@
-<?php
-function display_alert($message = "Invalid message", $location = "index.php")
-{
-    echo '<script type="text/javascript">
-                alert("' . $message . '");
-                window.location.href = "' . $location . '";
-                </script>';
-}
